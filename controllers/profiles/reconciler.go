70ed5be26fdc148bc50742a12a237717ad4fc489
Ricardo Zanini <zanini@redhat.com>
2023-02-09 17:19:20 -0300

    Add documentation and fix license headers
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- controllers/profiles/reconciler.go
+++ controllers/profiles/reconciler.go
@@ -25,7 +25,33 @@ import (
 	operatorapi "github.com/kiegroup/kogito-serverless-operator/api/v1alpha08"
 )
 
-// ProfileReconciler is the public interface to have access to this package and perform the actual reconciliation flow
+// ProfileReconciler is the public interface to have access to this package and perform the actual reconciliation flow.
+//
+// There are a few concepts in this package that you need to understand before attempting to maintain it:
+//
+// 1. ProfileReconciler: it's the main interface that internal structs implement via the baseReconciler.
+// Every profile must embed the baseReconciler.
+//
+// 2. stateSupport: is a struct with a few support objects passed around the reconciliation states like the client and logger.
+//
+// 3. reconciliationStateMachine: is a struct within the ProfileReconciler that do the actual reconciliation.
+// Each part of the reconciliation algorithm is a ReconciliationState that will be executed based on the ReconciliationState.CanReconcile call.
+//
+// 4. ReconciliationState: is where your business code should be focused on. Each state should react to a specific operatorapi.ConditionType.
+// The last conditions your state handles, the better. It can provide specific code that will only be triggered if the workflow is in that specific condition.
+//
+// 5. objectCreator: are functions to create a specific Kubernetes object based on a given workflow instance. This function should return the desired default state.
+//
+// 6. mutateVisitor: is a function that states can pass to objectEnsurer that will be applied to a given live object during the reconciliation cycle.
+// For example, if you wish to guarantee that an image in a specific container in the Deployment that you control and own won't change, make sure that your
+// mutate function guarantees that.
+//
+// 7. objectEnsurer: is a struct for a given objectCreator to control the reconciliation and merge conditions to an object.
+// A ReconciliationState may or may not have one or more ensurers. Depends on their role. There are states that just read objects, so no need to keep their desired state.
+//
+// See the already implemented reconciliation profiles to have a better understanding.
+//
+// While debugging, focus on the ReconciliationState(s), not in the profile implementation since the base algorithm is the same for every profile.
 type ProfileReconciler interface {
 	Reconcile(ctx context.Context, workflow *operatorapi.KogitoServerlessWorkflow) (ctrl.Result, error)
 	GetProfile() Profile
@@ -73,12 +99,12 @@ func (b baseReconciler) Reconcile(ctx context.Context, workflow *operatorapi.Kog
 	return result, err
 }
 
-// ReconciliationState is an interface implemented internally by different states to perform the adequate logic for a given workflow profile
+// ReconciliationState is an interface implemented internally by different reconciliation algorithms to perform the adequate logic for a given workflow profile
 type ReconciliationState interface {
-	// CanReconcile checks if this handler can perform its reconciliation task
+	// CanReconcile checks if this state can perform its reconciliation task
 	CanReconcile(workflow *operatorapi.KogitoServerlessWorkflow) bool
 	// Do perform the reconciliation task. It returns the controller result, the objects updated, and an error if any.
-	// Objects can be nil if the reconciliation task doesn't perform any updates in any Kubernetes object.
+	// Objects can be nil if the reconciliation state doesn't perform any updates in any Kubernetes object.
 	Do(ctx context.Context, workflow *operatorapi.KogitoServerlessWorkflow) (ctrl.Result, []client.Object, error)
 }
 
