c8646205d113b8a60eb1925d419b5d1f40f0f1ec
Davide Salerno <dsalerno@redhat.com>
2023-06-28 16:01:21 +0200

    Apply Marian's suggestions from code review
    Co-authored-by: Marián Macik <macik.marian@gmail.com>

--- api/v1alpha08/sonataflowbuild_types.go
+++ api/v1alpha08/sonataflowbuild_types.go
@@ -114,7 +114,7 @@ func (k *SonataFlowBuildStatus) GetInnerBuild(innerBuild interface{}) error {
 // +kubebuilder:printcolumn:name="Image",type=string,JSONPath=`.status.imageTag`
 // +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.buildPhase`
 // +kubebuilder:resource:shortName={"sfb", "sfbuild", "sfbuilds"}
-// +operator-sdk:csv:customresourcedefinitions:resources={{BuildConfig,build.openshift.io/v1," A Openshift Build Config"}}
+// +operator-sdk:csv:customresourcedefinitions:resources={{BuildConfig,build.openshift.io/v1,"An Openshift Build Config"}}
 type SonataFlowBuild struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
