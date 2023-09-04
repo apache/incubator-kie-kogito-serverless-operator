cde16c108f140977e5366f8b8cdcbc6cf2d0012f
Ricardo Zanini <zanini@redhat.com>
2023-09-04 16:37:44 -0300

    Fix property expansion problem
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- controllers/profiles/object_creators_test.go
+++ controllers/profiles/object_creators_test.go
@@ -17,40 +17,29 @@ package profiles
 import (
 	"testing"
 
-	"github.com/magiconair/properties"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
-
-	"github.com/kiegroup/kogito-serverless-operator/workflowproj"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/kiegroup/kogito-serverless-operator/test"
+	"github.com/kiegroup/kogito-serverless-operator/workflowproj"
 )
 
-func Test_ensureWorkflowPropertiesConfigMapMutator(t *testing.T) {
+func Test_ensureWorkflowPropertiesConfigMapMutator_DollarReplacement(t *testing.T) {
 	workflow := test.GetBaseSonataFlowWithDevProfile(t.Name())
-	// can't be new
-	cm, _ := workflowPropsConfigMapCreator(workflow)
-	cm.SetUID("1")
-	cm.SetResourceVersion("1")
-	reflectCm := cm.(*v1.ConfigMap)
+	existingCM := &v1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      workflow.Name,
+			Namespace: workflow.Namespace,
+			UID:       "0000-0001-0002-0003",
+		},
+		Data: map[string]string{
+			workflowproj.ApplicationPropertiesFileName: "mp.messaging.outgoing.kogito_outgoing_stream.url=${kubernetes:services.v1/event-listener}",
+		},
+	}
+	mutateVisitorFn := ensureWorkflowPropertiesConfigMapMutator(workflow, defaultProdApplicationProperties)
 
-	visitor := ensureWorkflowDevPropertiesConfigMapMutator(workflow)
-	mutateFn := visitor(cm)
-
-	assert.NoError(t, mutateFn())
-	assert.NotEmpty(t, reflectCm.Data[workflowproj.ApplicationPropertiesFileName])
-
-	props := properties.MustLoadString(reflectCm.Data[workflowproj.ApplicationPropertiesFileName])
-	assert.Equal(t, "8080", props.GetString("quarkus.http.port", ""))
-
-	// we change the properties to something different, we add ours and change the default
-	reflectCm.Data[workflowproj.ApplicationPropertiesFileName] = "quarkus.http.port=9090\nmy.new.prop=1"
-	visitor(reflectCm)
-	assert.NoError(t, mutateFn())
-
-	// we should preserve the default, and still got ours
-	props = properties.MustLoadString(reflectCm.Data[workflowproj.ApplicationPropertiesFileName])
-	assert.Equal(t, "8080", props.GetString("quarkus.http.port", ""))
-	assert.Equal(t, "0.0.0.0", props.GetString("quarkus.http.host", ""))
-	assert.Equal(t, "1", props.GetString("my.new.prop", ""))
+	err := mutateVisitorFn(existingCM)()
+	assert.NoError(t, err)
+	assert.Contains(t, existingCM.Data[workflowproj.ApplicationPropertiesFileName], "${kubernetes:services.v1/event-listener}")
 }
