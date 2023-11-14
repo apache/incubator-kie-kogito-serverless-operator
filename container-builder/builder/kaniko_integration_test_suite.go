48f0c3d7c97efe6cfde53099234148ef17fee286
Kennedy Bowers <kennedy.bowers@ibm.com>
2023-11-14 13:38:22 +0100

    running make generate-all and make vet fmt

--- container-builder/builder/kaniko_integration_test_suite.go
+++ container-builder/builder/kaniko_integration_test_suite.go
@@ -1,3 +1,5 @@
+//go:build integration_kaniko_docker
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-//go:build integration_kaniko_docker
 
 package builder
 
