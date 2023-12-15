cfc6bb4c0663fb97419fe4b70037af701d3e833f
Walter Medvedeo <wmedvedeo@gmail.com>
2023-12-15 09:07:51 +0100

    kie-kogito-serverless-operator-335: Operator driven service discovery API Phase4     - Add the discovery of Openshift DeploymentConfigs and Routes to the service discovery API

--- controllers/discovery/uri_parser.go
+++ controllers/discovery/uri_parser.go
@@ -63,7 +63,7 @@ func ParseUri(uri string) (*ResourceUri, error) {
 	} else if knativeSimplifiedServiceExpr.MatchString(uri) {
 		return parseKnativeSimplifiedServiceUri(uri)
 	} else if split := openshiftGroupsExpr.Split(uri, -1); len(split) == 2 {
-		return parseOpenshiftUri(openshiftGroupsExpr.FindString(uri), split[1])
+		return parseOpenshiftUri(uri, openshiftGroupsExpr.FindString(uri), split[1])
 	}
 	return nil, fmt.Errorf("invalid uri: %s, not correspond to any of the available schemes format: %s, %s, %s", uri, KubernetesScheme, KnativeScheme, OpenshiftScheme)
 }
@@ -165,6 +165,18 @@ func parseGVK(schemaGvk string) (*v1.GroupVersionKind, error) {
 			Version: "v1",
 			Kind:    "brokers",
 		}, nil
+	case openshiftRoutes:
+		return &v1.GroupVersionKind{
+			Group:   "route.openshift.io",
+			Version: "v1",
+			Kind:    "routes",
+		}, nil
+	case openshiftDeploymentConfigs:
+		return &v1.GroupVersionKind{
+			Group:   "apps.openshift.io",
+			Version: "v1",
+			Kind:    "deploymentconfigs",
+		}, nil
 	default:
 		return nil, fmt.Errorf("unknown schema and gvk: %s", schemaGvk)
 	}
@@ -212,6 +224,16 @@ func parseKnativeSimplifiedServiceUri(uri string) (*ResourceUri, error) {
 	}
 }
 
-func parseOpenshiftUri(findString string, s string) (*ResourceUri, error) {
-	return nil, fmt.Errorf("openshit is parsing not yet implemented")
+func parseOpenshiftUri(uri string, schemaAndGroup string, after string) (*ResourceUri, error) {
+	if namespace, name, gvk, queryParams, err := parseNamespaceNameGVKAndQueryParams(uri, schemaAndGroup, after); err != nil {
+		return nil, err
+	} else {
+		return &ResourceUri{
+			Scheme:      OpenshiftScheme,
+			GVK:         *gvk,
+			Namespace:   namespace,
+			Name:        name,
+			QueryParams: queryParams,
+		}, nil
+	}
 }
