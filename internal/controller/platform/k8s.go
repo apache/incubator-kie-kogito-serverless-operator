fedc48258b0e7ab16797aff19eae9a3480771f0c
rhkp <kpanchal@redhat.com>
2024-09-28 20:04:55 -0400

    Resolve merge conflicts

--- internal/controller/platform/k8s.go
+++ internal/controller/platform/k8s.go
@@ -61,6 +61,58 @@ func (action *serviceAction) CanHandle(platform *operatorapi.SonataFlowPlatform)
 	return platform.Status.IsReady()
 }
 
+// checkNReportInconsistentDBMigrationFlags emit warning in logs, if Job based migration is true and also DI/JS migrateDBOnStartUp is true, does not return error
+func (action *serviceAction) checkNReportInconsistentDBMigrationFlags(platform *operatorapi.SonataFlowPlatform, pshDI services.PlatformServiceHandler, pshJS services.PlatformServiceHandler) {
+	isJobBasedDBMigration := services.IsJobBasedDBMigration(platform)
+	diMigrateDBOnStartUp := false
+	jsMigrateDBOnStartUp := false
+	if pshDI.IsPersistenceSetInSpec() {
+		diMigrateDBOnStartUp = platform.Spec.Services.DataIndex.Persistence.MigrateDBOnStartUp
+	}
+
+	if pshJS.IsPersistenceSetInSpec() {
+		jsMigrateDBOnStartUp = platform.Spec.Services.JobService.Persistence.MigrateDBOnStartUp
+	}
+
+	if isJobBasedDBMigration && (diMigrateDBOnStartUp || jsMigrateDBOnStartUp) {
+		klog.V(log.W).InfoS("Inconsistent DB migration flags detected and it may cause unexpected errors or behaviours, please check SonataFlowPlatform deployment: ", "jobBasedDBMigration", isJobBasedDBMigration, "diMigrateDBOnStartUp", diMigrateDBOnStartUp, "jsMigrateDBOnStartUp", jsMigrateDBOnStartUp)
+	}
+}
+
+func (action *serviceAction) createOrUpdateDBMigrationJob(ctx context.Context, client client.Client, platform *operatorapi.SonataFlowPlatform, pshDI services.PlatformServiceHandler, pshJS services.PlatformServiceHandler) error {
+	action.checkNReportInconsistentDBMigrationFlags(platform, pshDI, pshJS)
+	dbMigratorJob, err := NewDBMigratorJobData(ctx, action.client, platform, pshDI, pshJS)
+	if err != nil {
+		klog.V(log.E).InfoS("Error extracting db-migration job data: ", "error", err)
+		return err
+	}
+	klog.V(log.I).InfoS("Extracted db-migation job data ")
+
+	job := dbMigratorJob.GetDBMigratorK8sJob(platform)
+	if op, err := controllerutil.CreateOrUpdate(ctx, client, job, func() error {
+		return nil
+	}); err != nil {
+		return err
+	} else {
+		klog.V(log.I).InfoS("DB Migration Job successfully created on cluster", "operation", op)
+	}
+
+	if err != nil {
+		klog.V(log.E).InfoS("Error executing db-migration job: ", "error", err)
+		return err
+	}
+	klog.V(log.I).InfoS("Got db-migration k8s job")
+
+	err = dbMigratorJob.MonitorCompletionOfDBMigratorJob(ctx, client, platform)
+	if err != nil {
+		klog.V(log.E).InfoS("Error monitoring completion of db-migration job: ", "error", err)
+		return err
+	}
+	klog.V(log.I).InfoS("Monitoring completed for db-migation k8s job")
+
+	return nil
+}
+
 func (action *serviceAction) Handle(ctx context.Context, platform *operatorapi.SonataFlowPlatform) (*operatorapi.SonataFlowPlatform, error) {
 	// Refresh applied configuration
 	if err := CreateOrUpdateWithDefaults(ctx, platform, false); err != nil {
@@ -68,13 +120,23 @@ func (action *serviceAction) Handle(ctx context.Context, platform *operatorapi.S
 	}
 
 	psDI := services.NewDataIndexHandler(platform)
+	psJS := services.NewJobServiceHandler(platform)
+
+	// Invoke DB Migration only if both or either DI/JS services are requested, in addition to jobBasedDBMigration
+	if services.IsJobBasedDBMigration(platform) && (psDI.IsServiceSetInSpec() || psJS.IsServiceSetInSpec()) {
+		klog.V(log.I).InfoS("Starting DB Migration Job: ")
+		err := action.createOrUpdateDBMigrationJob(ctx, action.client, platform, psDI, psJS)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	if psDI.IsServiceSetInSpec() {
 		if err := createOrUpdateServiceComponents(ctx, action.client, platform, psDI); err != nil {
 			return nil, err
 		}
 	}
 
-	psJS := services.NewJobServiceHandler(platform)
 	if psJS.IsServiceSetInSpec() {
 		if err := createOrUpdateServiceComponents(ctx, action.client, platform, psJS); err != nil {
 			return nil, err
